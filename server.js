cb9d0f16bcecfbab85a027c82746edc6f43947eb
Atchayavijay <atchuvijay83@gmail.com>
2025-01-03 20:13:01 +0530

    split the server file into app

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-const express = require('express');
+
 const mongoose = require('mongoose');
 const { MONGODB_URI, PORT } = require('./utils/config');
+const app =require('./app');
 
-const app =express();
 
-app.get('/', (request,response) => {
-    response.json({message:"Hello world"});
-});
-app.listen(PORT,()=>{
-    console.log('Server is running @ http://localhost:3001')
-});
 
 mongoose.connect(MONGODB_URI)
 .then(()=>{
-    console.log("Connect to the database....")
+    console.log("Connect to the database....");
+    app.listen(PORT,()=>{
+        console.log('Server is running @ http://localhost:3001')
+    });
 })
 .catch(()=>{
-    console.log("Error connecting to MongoDb........",error)
+    console.log("Error connecting to MongoDb..",error)
 });

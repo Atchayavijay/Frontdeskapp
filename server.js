64f1501c234c6617b4507149677f7205f921ee80
Atchayavijay <atchuvijay83@gmail.com>
2025-01-03 19:24:22 +0530

    express installization

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+const express = require('express');
+const mongoose = require('mongoose')
+
+const app =express();
+
+app.get('/', (request,response) => {
+    response.json({message:"Hello world"});
+});
+app.listen(3001,()=>{
+    console.log('Server is running @ http://localhost:3001')
+});
+

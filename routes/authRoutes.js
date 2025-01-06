b873c324b4ed9ae37c2fc33304b7be4bfa53af1c
Atchayavijay <atchuvijay83@gmail.com>
2025-01-06 19:13:59 +0530

    login setup

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,6 +4,9 @@ const authController = require('../controllers/authControllers')
 const authRouter = express.Router();
 
 authRouter.post('/register', authController.register);
+authRouter.post('/login', authController.login);
+authRouter.post('/logout', authController.logout);
+
 
 
  module.exports =authRouter;
